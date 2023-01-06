a4a11fa9a137f53d5a75faff57c575e214c48643
dpoor <dpoor@192.168.2.13>
2023-01-05 23:20:17 -0500

    commit2

---
+++ GitHubProj2/src/main/java/package2/Class2.java
@@ -0,0 +1,11 @@
+package package2;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		System.out.print("This is my 2nd project");
+		// TODO Auto-generated method stub
+
+	}
+
+}
